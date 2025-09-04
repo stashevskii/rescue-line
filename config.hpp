65665ab1e5fbf53e02b1c653acbe71f0e1762383
alex <staleksandr05@gmail.com>
2025-09-04 18:56:53 +0300

    added config

---
+++ config.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// Motor Right
+constexpr int MOTOR_RIGHT_A_IN1 = 31;
+constexpr int MOTOR_RIGHT_A_IN2 = 29;
+constexpr int MOTOR_RIGHT_A_PWM = 44;
+
+constexpr int MOTOR_RIGHT_B_IN1 = 27;
+constexpr int MOTOR_RIGHT_B_IN2 = 25;
+constexpr int MOTOR_RIGHT_B_PWM = 46;
+
+// Motor Left
+constexpr int MOTOR_LEFT_A_IN1 = 9;
+constexpr int MOTOR_LEFT_A_IN2 = 11;
+constexpr int MOTOR_LEFT_A_PWM = 6;
+
+constexpr int MOTOR_LEFT_B_IN1 = 5;
+constexpr int MOTOR_LEFT_B_IN2 = 13;
+constexpr int MOTOR_LEFT_B_PWM = 7;
+
+// LEDs
+constexpr int LED1 = 34;
+constexpr int LED2 = 36;
+constexpr int LED3 = 38;
+
+// Buttons
+constexpr int BTN_SET = 12;
+constexpr int BTN_PLUS = 15;
+constexpr int BTN_MINUS = 14;

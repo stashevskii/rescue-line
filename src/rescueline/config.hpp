2ec6b0e18e68878b178c6904485768977615e7d6
alex <staleksandr05@gmail.com>
2025-09-05 19:17:41 +0300

    add basic motor control

--- src/rescueline/config.hpp
+++ src/rescueline/config.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 // Motor Right
-constexpr int MOTOR_RIGHT_A_IN1 = 31;
-constexpr int MOTOR_RIGHT_A_IN2 = 29;
+constexpr int MOTOR_RIGHT_A_IN1 = 29;
+constexpr int MOTOR_RIGHT_A_IN2 = 31;
 constexpr int MOTOR_RIGHT_A_PWM = 44;
 
-constexpr int MOTOR_RIGHT_B_IN1 = 27;
-constexpr int MOTOR_RIGHT_B_IN2 = 25;
+constexpr int MOTOR_RIGHT_B_IN1 = 25;
+constexpr int MOTOR_RIGHT_B_IN2 = 27;
 constexpr int MOTOR_RIGHT_B_PWM = 46;
 
 // Motor Left
@@ -14,9 +14,33 @@ constexpr int MOTOR_LEFT_A_IN1 = 9;
 constexpr int MOTOR_LEFT_A_IN2 = 11;
 constexpr int MOTOR_LEFT_A_PWM = 6;
 
-constexpr int MOTOR_LEFT_B_IN1 = 5;
-constexpr int MOTOR_LEFT_B_IN2 = 13;
+constexpr int MOTOR_LEFT_B_IN1 = 13;
+constexpr int MOTOR_LEFT_B_IN2 = 5;
 constexpr int MOTOR_LEFT_B_PWM = 7;
+
+constexpr int MOTOR_PINS[] = {
+  // Motor Right A
+  MOTOR_RIGHT_A_IN1,
+  MOTOR_RIGHT_A_IN2,
+  MOTOR_RIGHT_A_PWM,
+  
+  // Motor Right B
+  MOTOR_RIGHT_B_IN1,
+  MOTOR_RIGHT_B_IN2,
+  MOTOR_RIGHT_B_PWM,
+  
+  // Motor Left A
+  MOTOR_LEFT_A_IN1,
+  MOTOR_LEFT_A_IN2,
+  MOTOR_LEFT_A_PWM,
+
+  // Motor Left B
+  MOTOR_LEFT_B_IN1,
+  MOTOR_LEFT_B_IN2,
+  MOTOR_LEFT_B_PWM 
+};
+
+constexpr size_t MOTOR_PINS_COUNT = sizeof(MOTOR_PINS) / sizeof(MOTOR_PINS[0]);
 
 // LEDs
 constexpr int LED1 = 34;
--- src/rescueline/rescueline.ino
+++ src/rescueline/rescueline.ino
@@ -1,9 +1,43 @@
-#include "config.h"
+#include "config.hpp"
+
+void setMotor(int speed, int pin1, int pin2, int pwmPin) {
+  speed = constrain(speed, -255, 255);
+  if (speed >= 0) {
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, LOW);
+    analogWrite(pwmPin, abs(speed));
+  } else {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, HIGH);
+    analogWrite(pwmPin, abs(speed));
+  }
+}
+
+void driveFront(int rightSpeed, int leftSpeed) {
+  setMotor(rightSpeed, MOTOR_RIGHT_A_IN1, MOTOR_RIGHT_A_IN2, MOTOR_RIGHT_A_PWM);
+  setMotor(leftSpeed, MOTOR_LEFT_A_IN1, MOTOR_LEFT_A_IN2, MOTOR_LEFT_A_PWM);
+}
+
+void driveBack(int rightSpeed, int leftSpeed) {
+  setMotor(rightSpeed, MOTOR_RIGHT_B_IN1, MOTOR_RIGHT_B_IN2, MOTOR_RIGHT_B_PWM);
+  setMotor(leftSpeed, MOTOR_LEFT_B_IN1, MOTOR_LEFT_B_IN2, MOTOR_LEFT_B_PWM);
+}
+
+void standBy() {
+  pinMode(30, OUTPUT);
+  digitalWrite(30, HIGH);
+}
 
 void setup() {
-
+  for (int i = 0; i < 12; i++) {
+    pinMode(MOTOR_PINS[i], OUTPUT);
+  }
+  standBy();
 }
 
 void loop() {
-  
+  setMotor(128, MOTOR_RIGHT_A_IN1, MOTOR_RIGHT_A_IN2, MOTOR_RIGHT_A_PWM);
+  setMotor(128, MOTOR_LEFT_A_IN1, MOTOR_LEFT_A_IN2, MOTOR_LEFT_A_PWM);
+  setMotor(128, MOTOR_RIGHT_B_IN1, MOTOR_RIGHT_B_IN2, MOTOR_RIGHT_B_PWM);
+  setMotor(128, MOTOR_LEFT_B_IN1, MOTOR_LEFT_B_IN2, MOTOR_LEFT_B_PWM);
 }

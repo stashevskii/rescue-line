d30709b25ed2324b196adfd2e09d996fab6c72a8
Leshechkagit <leshash08@gmail.com>
2025-11-19 19:10:39 +0300

    add lineFollow void

--- src/rescueline/rescueline.ino
+++ src/rescueline/rescueline.ino
@@ -3,35 +3,37 @@
 
 constexpr int BAUD_RATE = 19200;
 constexpr int CHAR_BUF = 128;
-constexpr double KP = 3;
-constexpr int BASIC_SPEED = 45;
+constexpr double KP = 0.1;
+constexpr double KD = 0.8;
+constexpr int BASIC_SPEED = 100;
 constexpr int N = 4;
 constexpr int w[N] = {1000, 2000, 3000, 4000};
-constexpr int linePins[N] = {A14, A6, A4, A8};
+constexpr int linePins[N] = {A14, A8, A6, A4};
 int lineValues[N];
 double err;
 int blobPos;
 int isCross;
+int lastPos = 2500;
 
 void setMotor(int speed, int pin1, int pin2, int pwmPin) {
-  speed = constrain(speed, -255, 255);
+  speed = constrain(speed, -100, 200);
   if (speed >= 0) {
     digitalWrite(pin1, HIGH);
     digitalWrite(pin2, LOW);
     analogWrite(pwmPin, abs(speed));
-  } else {
+  } else { 
     digitalWrite(pin1, LOW);
     digitalWrite(pin2, HIGH);
     analogWrite(pwmPin, abs(speed));
   }
 }
 
-void driveFront(int rightSpeed, int leftSpeed) {
+void driveFront(int leftSpeed, int rightSpeed) {
   setMotor(rightSpeed, MOTOR_RIGHT_A_IN1, MOTOR_RIGHT_A_IN2, MOTOR_RIGHT_A_PWM);
   setMotor(leftSpeed, MOTOR_LEFT_A_IN1, MOTOR_LEFT_A_IN2, MOTOR_LEFT_A_PWM);
 }
 
-void driveBack(int rightSpeed, int leftSpeed) {
+void driveBack(int leftSpeed, int rightSpeed) {
   setMotor(rightSpeed, MOTOR_RIGHT_B_IN1, MOTOR_RIGHT_B_IN2, MOTOR_RIGHT_B_PWM);
   setMotor(leftSpeed, MOTOR_LEFT_B_IN1, MOTOR_LEFT_B_IN2, MOTOR_LEFT_B_PWM);
 }
@@ -40,7 +42,10 @@ void readSensors() {
   for (int i = 0; i < N; ++i) {
     lineValues[i] = analogRead(linePins[i]);
   }
-}
+  for (int i = 0; i < N; ++i) {
+    lineValues[i] = (lineValues[i] + analogRead(linePins[i]))/2;
+  }
+} 
 
 void printSensors() {
   readSensors();
@@ -53,18 +58,41 @@ void printSensors() {
   Serial.println();
 }
 
-double getNormalized() {
-  int s = 0, s2 = 0;
+int readLine() {
+  int s = 0;
+  long s2 = 0;
+  bool onLine = false;
   readSensors();
   for (int i = 0; i < N; ++i) {
-    s += lineValues[i];
-    s2 += lineValues[i] * w[i];
+    if(lineValues[i] > 150){
+      s += lineValues[i];
+      s2 += lineValues[i] * (long)w[i];
+      onLine = true;
+    }
   }
-  return (double)s2 / s;
+  if(!onLine){
+    if(lastPos<2500){
+      return 0;
+    }else{
+      return 5000;
+    }
+  }
+  lastPos = s2 / s;
+  return lastPos;
 }
 
-double getError() {
-  return 2500.0 - getNormalized();
+
+void LFR(){
+  int lastError = 0;
+  while(true){
+  int err = 2500 - readLine();
+  int delta =(err*KP) + (err - lastError)*KD;
+  lastError = err;
+  // printSensors();
+  Serial.println(delta);
+  driveFront(BASIC_SPEED - delta, BASIC_SPEED + delta);
+  driveBack(BASIC_SPEED - delta, BASIC_SPEED + delta);
+  }
 }
 
 void standBy() {
@@ -81,11 +109,14 @@ void setup() {
   Serial2.begin(BAUD_RATE);
 }
 
+void testDrive(){
+  while(true){
+    driveFront(0, 100);
+    driveBack(0, 100);
+  }
+}
+
 void loop() {
-  printSensors();
-  double err = getError();
-  err *= KP;
-  delay(400);
-  /*driveFront(BASIC_SPEED + err, BASIC_SPEED - err);
-  driveBack(BASIC_SPEED + err, BASIC_SPEED - err);*/
+  LFR();
+  // testDrive();
 }
